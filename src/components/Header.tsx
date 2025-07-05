4cdd23792883f9a9811147755561d00e5fa4bcf8
Eslam AlBaik <89574402+eslamalbaik@users.noreply.github.com>
2025-07-05 15:22:18 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -92,7 +92,7 @@ useEffect(() => {
               <Phone className="w-4 h-4" />
             </div>
           </div> */}
-        <a href="#_" className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-[#560CAB] rounded-full shadow-md group">
+        <a href="#consultation" className="relative hidden lg:flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-[#560CAB] rounded-full shadow-md group">
             <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-[#560CAB] group-hover:translate-x-0 ease">
               <Phone className="w-4 h-4" />
             </span>
